3ea8dea314d9d268d49a9352db176ad49000dde5
UngVanilje <103105638+UngVanilje@users.noreply.github.com>
2022-09-20 08:30:25 +0200

    5

--- Lektion 6/src/Test/JAVAexamples.java
+++ Lektion 6/src/Test/JAVAexamples.java
@@ -21,14 +21,23 @@ public class JAVAexamples {
 	public static void selectudenparm() {
 	try {
 		// Laver sql-sætning og får den udført
-		String sql = "select navn,stilling from person";
+		String sql = "select init, aarstal, plac from placering";
 		System.out.println("SQL-streng er "+sql);
 		ResultSet res=stmt.executeQuery(sql);
 		// gennemløber svaret
 		while (res.next()) {
-			String s;
-			s = res.getString("navn");
-			System.out.println(s + "    " + res.getString(2));
+			String init;
+			int aarstal;
+			int plac;
+			init = res.getString("init");
+			aarstal = res.getInt(2);
+			plac = res.getInt(3);
+			if (plac > 0)
+				System.out.println("Initialer: " + init + "    " + "Placering: " + plac + "   		" + "Årstal: " +  aarstal);
+			else
+			{
+				System.out.println("Initialer: " + init + "    " + "Placering: UDGÅET" + "   	" + "Årstal: " +  aarstal);
+			}
 		}
 		// pæn lukning
  		if (!minConnection.isClosed()) minConnection.close();
@@ -44,12 +53,12 @@ public class JAVAexamples {
 		System.out.println("Indtast søgestreng");
 		String inString = inLine.readLine();
 		// Laver sql-sætning og får den udført
-		String sql = "select navn,stilling from person where navn like '" + inString + "%'";
+		String sql = "select init, rytternavn from rytter where init like '" + inString + "%'";
 		System.out.println("SQL-streng er "+ sql);
 		ResultSet res=stmt.executeQuery(sql);
 		//gennemløber svaret
 		while (res.next()) {
-			System.out.println(res.getString(1) + "    " + res.getString(2));
+			System.out.println("Initialer:" + res.getString(1) + "    " + "Rytternavn:" + res.getString(2));
 		}
 		// pæn lukning
  		if (!minConnection.isClosed()) minConnection.close();
@@ -59,37 +68,38 @@ public class JAVAexamples {
 		}
 	}
 	
-	public static void insertmedstring() {
+	public static void insertmedstringRytter() {
 		try {
 			// indlæsning
-			System.out.println("Vi vil nu oprette et nyt ansættelsesfforhold");
-			System.out.println("Indtast cpr (personen skal være oprettet på forhånd");
-			String cprstr=inLine.readLine();
-			System.out.println("Indtast firmanr (firma skal være oprettet på forhånd");
-			String firmastr=inLine.readLine();
+			System.out.println("Vi vil nu oprette en ny rytter");
+			System.out.println("Indtast rytter initialer (rytter skal være oprettet på forhånd");
+			String initnavnstr=inLine.readLine();
+			System.out.println("Indtast rytter rytternavn (rytter skal være oprettet på forhånd");
+			String rytternavnstr=inLine.readLine();
+
 		
 			// sender insert'en til db-serveren
-			String sql = "insert into ansati values ('" + cprstr + "'," + firmastr + ")";
+			String sql = "insert into rytter values ('" + initnavnstr + "','" + rytternavnstr + "')";
 			System.out.println("SQL-streng er "+ sql);
 			stmt.execute(sql);
 			// pænt svar til brugeren
-			System.out.println("Ansættelsen er nu registreret");
+			System.out.println("Rytteren er nu registreret");
 			if (!minConnection.isClosed()) minConnection.close();
 		}
 		catch (SQLException e) {
 			        switch (e.getErrorCode())
 			        // fejl-kode 547 svarer til en foreign key fejl
-			        { case 547 : {if (e.getMessage().contains("cprforeign"))
-										System.out.println("cpr-nummer er ikke oprettet");
+			        { case 547 : {if (e.getMessage().contains("initrytterforeign"))
+										System.out.println("rytter initialer er ikke oprettet");
 			        			  else
-			        			  if (e.getMessage().contains("firmaforeign"))
-										System.out.println("firmaet er ikke oprettet");
+			        			  if (e.getMessage().contains("rytternavnforeign"))
+										System.out.println("rytternavn er ikke oprettet");
 			        			  else
 			        				    System.out.println("ukendt fremmednøglefejl");
 								  break;
 			        			}
 			        // fejl-kode 2627 svarer til primary key fejl
-			          case 2627: {System.out.println("den pågældende ansættelse er allerede oprettet");
+			          case 2627: {System.out.println("Den pågældende rytter er allerede oprettet");
 			          	          break;
 			         			 }
 			          default: System.out.println("fejlSQL:  "+e.getMessage());
@@ -99,45 +109,98 @@ public class JAVAexamples {
 			System.out.println("fejl:  "+e.getMessage());
 		}
 	};
+	public static void insertmedstringPlacering() {
+		try {
+			// indlæsning
+			System.out.println("Vi vil nu oprette en ny placering i året 2021");
+			System.out.println("Indtast årstal (placering skal være oprettet på forhånd");
+			String aarstalnavnstr=inLine.readLine();
+			System.out.println("Indtast rytter initialer (placering skal være oprettet på forhånd");
+			String initnavnstr=inLine.readLine();
+			System.out.println("Indtast placering (placering skal være oprettet på forhånd");
+			String placnavnstr=inLine.readLine();
+//			System.out.println("Indtast firmanr (placering skal være oprettet på forhånd");
+//			String firmastr=inLine.readLine();
+
+			// sender insert'en til db-serveren
+			String sql = "insert into placering values ('" + aarstalnavnstr + "','" + initnavnstr + "','" + placnavnstr + "')";
+			System.out.println("SQL-streng er "+ sql);
+			stmt.execute(sql);
+			// pænt svar til brugeren
+			System.out.println("Placering er nu registreret");
+			if (!minConnection.isClosed()) minConnection.close();
+		}
+		catch (SQLException e) {
+			switch (e.getErrorCode())
+			// fejl-kode 547 svarer til en foreign key fejl
+			{ case 547 : {if (e.getMessage().contains("initforeign"))
+				System.out.println("rytter initialer er ikke oprettet");
+			else
+			if (e.getMessage().contains("placforeign"))
+				System.out.println("placering er ikke oprettet");
+			else
+			if (e.getMessage().contains("aarstalforeign"))
+				System.out.println("aarstal er ikke oprettet");
+			else
+				System.out.println("ukendt fremmednøglefejl");
+				break;
+			}
+			// fejl-kode 2627 svarer til primary key fejl
+				case 2627: {System.out.println("den pågældende rytter er allerede oprettet");
+					break;
+				}
+				default: System.out.println("fejlSQL:  "+e.getMessage());
+			};
+		}
+		catch (Exception e) {
+			System.out.println("fejl:  "+e.getMessage());
+		}
+	};
 	
 	public static void insertprepared() {
 		try {
 			// indl�sning
-			System.out.println("Vi vil nu oprette et nyt ansættelsesforhold");
-			System.out.println("Indtast cpr (personen skal være oprettet på forhånd");
-			String cprstr=inLine.readLine();
-			System.out.println("Indtast firmanr (firma skal være oprettet på forhånd");
-			String firmastr=inLine.readLine();
+			System.out.println("Indtast aarstal (aarstal skal være oprettet på forhånd");
+			String aarstalstr=inLine.readLine();
+			System.out.println("Indtast initialer (rytteren skal være oprettet på forhånd");
+			String initstr=inLine.readLine();
+			System.out.println("Indtast placering (rytteren skal være oprettet på forhånd");
+			String placstr=inLine.readLine();
 			// Anvendelse af prepared statement
-			String sql = "insert into ansati values (?,?)";
+			String sql = "insert into placering values (?,?,?)";
 			PreparedStatement prestmt = minConnection.prepareStatement(sql);
 			prestmt.clearParameters();
-			prestmt.setString(1,cprstr);
-			prestmt.setInt(2,Integer.parseInt(firmastr));
+			prestmt.setInt(1,Integer.parseInt(aarstalstr));
+			prestmt.setString(2,initstr);
+			prestmt.setInt(3,Integer.parseInt(placstr));
+
 			// Udf�rer s�tningen
 			prestmt.execute();
 			// p�nt svar til brugeren
-			System.out.println("Ansættelsen er nu registreret");
+			System.out.println("Placering er nu registreret");
 			if (!minConnection.isClosed()) minConnection.close();
 		}
 		catch (SQLException e) {
-			        switch (e.getErrorCode())
-			        // fejl-kode 547 svarer til en foreign key fejl
-			        { case 547 : {if (e.getMessage().contains("cprforeign"))
-									  System.out.println("cpr-nummer er ikke oprettet");
-			        			  else	
-			        			  if (e.getMessage().contains("firmaforeign"))
-						              System.out.println("firmaet er ikke oprettet");
-			        			  else
-			        				  System.out.println("ukendt fremmednøglefejl");
-								  break;
-			        			}
-			        // fejl-kode 2627 svarer til primary key fejl
-			          case 2627: {System.out.println("den pågældende ansættelse er allerede oprettet");
-			          	          break;
-			         			 }
-			          default: System.out.println("fejlSQL:  "+e.getMessage());
-				};
+			switch (e.getErrorCode())
+			// fejl-kode 547 svarer til en foreign key fejl
+			{ case 547 : {if (e.getMessage().contains("initforeign"))
+				System.out.println("rytter initialer er ikke oprettet");
+			else
+			if (e.getMessage().contains("placforeign"))
+				System.out.println("placering er ikke oprettet");
+			else
+			if (e.getMessage().contains("aarstalforeign"))
+				System.out.println("aarstal er ikke oprettet");
+			else
+				System.out.println("ukendt fremmednøglefejl");
+				break;
+			}
+			// fejl-kode 2627 svarer til primary key fejl
+				case 2627: {System.out.println("den pågældende rytter er allerede oprettet");
+					break;
+				}
+				default: System.out.println("fejlSQL:  "+e.getMessage());
+			};
 		}
 		catch (Exception e) {
 			System.out.println("fejl:  "+e.getMessage());
@@ -152,25 +215,27 @@ public class JAVAexamples {
 			//via native driver
 			String server="localhost\\SQLEXPRESS"; //virker måske hos dig
 			                                       //virker det ikke - prøv kun med localhost
-			String dbnavn="eksempeldb";            //virker måske hos dig 
+			String dbnavn="cykeldb";            //virker måske hos dig
 			String login="sa";                     //skal ikke ændres
-			String password="torben07";            //skal ændres 
+			String password="Emil1996";            //skal ændres
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 			minConnection = DriverManager.getConnection("jdbc:sqlserver://"+server+";databaseName="+dbnavn+
 					";user=" + login + ";password=" + password + ";");
-			//minConnection = DriverManager.getConnection("jdbc:sqlserver://localhost\\SQLEXPRESS;databaseName=eksempeldb;user=sa;password=torben07;");
+			//minConnection = DriverManager.getConnection("jdbc:sqlserver://localhost\\SQLEXPRESS;databaseName=eksempeldb;user=sa;password=Emil1996;");
 			stmt = minConnection.createStatement();
 			//Indlæsning og kald af den rigtige metode
 			System.out.println("Indtast  "); 
 			System.out.println("s for select uden parameter  ");
 			System.out.println("sp for select med parameter  ");
-			System.out.println("i for insert med strengmanipulation");
+			System.out.println("ip for insert med strengmanipulation placering");
+			System.out.println("ir for insert med strengmanipulation rytter");
 			System.out.println("ps for insert med prepared statement ");
 			String in=inLine.readLine();
 			switch (in)
 			{case "s"  : {selectudenparm();break;}
 			 case "sp" : {selectmedparm();break;}
-			 case "i"  : {insertmedstring();break;}
+			 case "ip"  : {insertmedstringPlacering();break;}
+				case "ir" : {insertmedstringRytter();break;}
 			 case "ps"  : {insertprepared();break;}
 			default : System.out.println("ukendt indtastning"); 
 			} 
@@ -179,7 +244,8 @@ public class JAVAexamples {
 			System.out.println("fejl:  "+e.getMessage());
 		}
 	}
-	
+
+
 
 }
 
